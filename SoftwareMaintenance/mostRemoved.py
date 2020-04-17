b54416b3a9aed2540123919ebacb7f8a44c6f46b
Sebastianxq <Sebastianq1290@gmail.com>
2020-04-16 22:43:03 -0500

    Function to enumerate LOC added done

--- SoftwareMaintenance/mostRemoved.py
+++ SoftwareMaintenance/mostRemoved.py
@@ -30,18 +30,22 @@ users = {}
 #dict.update(newkey = 'value')
 count = 0
 
-##FIX: Count discrepancy?
+
 for commit in RepositoryMining(project_url).traverse_commits():
 	for m in commit.modifications:
 		count += m.added
-		if (commit.author.name in users):
+		if (commit.author.name not in users):
+			print("only here once")
+			print (commit.author.name)
+			name = str(commit.author.name)
+			linesAdded = m.added
+			userAdd = {commit.author.name: m.added}
+			users.update(userAdd)
+		else:
 			temp = users[commit.author.name]
 			temp += m.added
 			users[commit.author.name] = temp
-		else:
-			name = commit.author.name
-			linesAdded = m.added
-			users.update(name = linesAdded)
+			
 		
 print("done with loop")
 print(users)
