ab3ee4b9222f1ed7c242394eab9b39c1e2c7b594
Sebastianxq <Sxquinones@miners.utep.edu>
2020-10-03 23:21:03 -0400

    Fixed multi-file reading, other various improvements

--- InfoRetrieval/InvertedIndex/ii.py
+++ InfoRetrieval/InvertedIndex/ii.py
@@ -1,22 +1,65 @@
-#Go through file 1, store in dictionary
-#GO through file 2, store in dictionary, if duplicate, append to value
-#...
-#Done
+import os
 
-def indexGenerator():
+index = {} #global dict for storing words
+
+def normalization(word):
+	table = str.maketrans(dict.fromkeys(" ,.;: ")) #key used to remove punctuation from words
+	word = word.lower()          #turns all letters lowercase
+	word = word.translate(table) #removes most punctuation 
+	word = word.replace('"', '') #removes quotations from words
+	#if "-"  in word: #splits hyphenated words                    
+    #elif "'"  in word:  #splits concatenated words
+               
+
+def index2():
 	index = {}
-	with open("data/d1.txt", "r") as file:
-		for line in file: 
-			# reading each word         
-			for word in line.split():
-				#check if word doesnt exist and if so add it and add "d1" as the key
-				if word not in index:
-					index[word] = "d1"
+	directory = os.fsencode("data")
+	#print(os.path.splitext(file)[0])
+
+	for file in os.listdir(directory):
+		#print(os.path.splitext(file)[0].decode())
+
+		filename = os.path.splitext(file)[0].decode()
+		with open("data/"+filename+".txt", "r") as file:
+
+			for line in file:          
+				for word in line.split():
+					print(type(word))
+					#if word not in index:
+					#	index[word].append(filename,)
 	print(index)
 
-    #In the future find a way to read over all the files in one loop,  
-   
+def indexGenerator(inputFile):
+	global index #brings index in-scope
+	with open(inputFile, "r") as file:
+		fileNum = inputFile[5:7]
+		#print(fileNum) #DEBUG
+		for line in file:    
+			for word in line.split():
+				normalization(word) #Break this up later
+				if word not in index:
+
+					#Overwrites instead of appends
+					index[word] = (fileNum,)
+	#print(index)
+
+
+#returns a list of all files
+def getAllFiles(dirName):
+	directory = os.fsencode(dirName)
+	files = []
+	for file in os.listdir(directory):
+		files.append(file.decode())
+
+		#used if we just want the doc name, not the extension
+		#filename = os.path.splitext(file)[0].decode()	
+	return files	   
+
 # Main function
 if __name__ == '__main__':
-
-	indexGenerator()
+	dirName = "data/" #Later make this an input
+	files = getAllFiles(dirName)
+	#print(files) #DEBUG
+	for file in files:
+		indexGenerator(dirName+file)
+	print(index)
