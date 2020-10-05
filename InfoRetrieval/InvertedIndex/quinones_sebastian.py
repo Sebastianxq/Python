e19af9df7cdf8bcbf89e8a22cf3df7bd060f8f20
Sebastianxq <Sxquinones@miners.utep.edu>
2020-10-05 16:41:19 -0400

    fixd logic error in OR algorithm

--- InfoRetrieval/InvertedIndex/quinones_sebastian.py
+++ InfoRetrieval/InvertedIndex/quinones_sebastian.py
@@ -2,7 +2,7 @@ import os
 import re
 import sys
 from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
+
 index = {} #global dict for storing words
 
 def normalization(word):
@@ -10,14 +10,13 @@ def normalization(word):
 
 	table = str.maketrans(dict.fromkeys(" ,.;:'()“”% ")) #key used to remove punctuation from words
 	word = word.lower()          #turns all letters lowercase
-	word = word.translate(table) #removes most punctuation 
-	word = word.strip('"') #removes special quotations from words
-	word = word.replace('-', ' ')
-	word = word.replace('---', ' ') #literally for one set of words
+	word = word.translate(table) #removes punctuation that doesnt need to be replaced with a space
+	word = word.strip('"') 		 #removes quotations
+	word = word.replace('-', ' ')   #other special character cases
+	word = word.replace('---', ' ')
 
 	word = ps.stem(word) #Stems words (Cuts off prefix and suffix)
 
-	#Might have to add something that removes hyphens later.
 	return word               
 
 
@@ -25,11 +24,11 @@ def normalization(word):
 def indexGenerator(inputFile):
 	global index #brings index in-scope
 	with open(inputFile, "r") as file:
-		fileNum = inputFile[5:7]
+		fileNum = inputFile[5:7] #value for the dictionary
 		#print(fileNum) #DEBUG
 		for line in file:    
 			for word in line.split():
-				word = normalization(word) #Removes certain special chars
+				word = normalization(word) #Cleans up word
 				#print(word) #DEBUG
 				if word in index: #If word already in index, ensure that we don't enter a duplicate document
 					#print("word was already in index") #DEBUG
@@ -69,18 +68,15 @@ def indexOutput():
 			printStatement = key+"  "+docStrings+'\n'
 			file.write(printStatement)	
 
+#runs queries on Invertes Index
 def queryCheck(filename):
-	#get query
-	#Turn both of the words into a postings list
-	#AND or OR accordingly (Using the algorithm in class)
-
 	queryFile = open(filename,"r")
 	queries = queryFile.readlines() 
 	queryValue = 1
 	for query in queries:
 		queryParts = query.split()
 
-		#Querys succesfully get their algorithm selected
+		#Based on query, run the required algorithm
 		if queryParts[1] == "AND":
 			#print("queryPart is %s"%(queryParts[1]))
 			answer = andAlgorithm(queryParts[0],queryParts[2])
@@ -93,17 +89,19 @@ def queryCheck(filename):
 			print("#%d Query %s Results: %s\n" %(queryValue, query, answer))
 			queryValue+=1
 
+#returns documents where both words are present
 def andAlgorithm(word1, word2):
 	global index
 
 	#If either word doesnt have a posting available, automatically cancel
+	#Note: Both lists must also be ordered as per the algorithm requirements
 	try:
 		p1 = index[word1.lower()]
 		p2 = index[word2.lower()]
 		p1.sort(key=lambda x:x[1])
 		p2.sort(key=lambda x:x[1])
-		print("word1 %s \t posting1:%s" %(word1, p1)) #DEBUG
-		print("word2 %s \t posting2:%s" %(word2, p2)) #DEBUG
+		#print("word1 %s \t posting1:%s" %(word1, p1)) #DEBUG
+		#print("word2 %s \t posting2:%s" %(word2, p2)) #DEBUG
 	except:
 		print("%s or %s not found in index" %(word1,word2)) #DEBUG
 		return -1
@@ -135,34 +133,47 @@ def andAlgorithm(word1, word2):
 	return answer
 
 
+#Returns documents where at least one word is present
 def orAlgorithm(word1, word2):
 	global index
-
-	#If either word doesnt have a posting available, automatically cancel
-	try:
-		p1 = index[word1.lower()]
-		p2 = index[word2.lower()]
-		print("word1 %s \t posting1:%s" %(word1, p1)) #DEBUG
-		print("word2 %s \t posting2:%s" %(word2, p2)) #DEBUG
-	except:
-		print("%s or %s not found in index" %(word1,word2)) #DEBUG
-		return -1
-
 	answer = []
 	p1Index = 0
 	p2Index = 0
 
-	#All values in p1 are valid
-	while p1Index < len(p1):
-		if p1[p1Index] not in answer: 
-			answer.append(p1[p1Index])
-		p1Index+=1
+	#Split up into 2 tries in the event that one posting is empty but the other isn't
+	try:
+		p1 = index[word1.lower()]
 
-	#All values in p2 are valid
-	while p2Index < len(p2):
-		if p2[p2Index] not in answer: 
-			answer.append(p2[p2Index])
-		p2Index+=1
+		#All values in p1 are valid
+		while p1Index < len(p1):
+			if p1[p1Index] not in answer: 
+				answer.append(p1[p1Index])
+			p1Index+=1
+		p2 = index[word2.lower()]
+		#All values in p2 are valid
+		while p2Index < len(p2):
+			if p2[p2Index] not in answer: 
+				answer.append(p2[p2Index])
+			p2Index+=1
+		#print("word1 %s \t posting1:%s" %(word1, p1)) #DEBUG
+		#print("word2 %s \t posting2:%s" %(word2, p2)) #DEBUG
+	except:
+		print("%s not found in index" %(word1)) #DEBUG
+
+	try:
+		p2 = index[word2.lower()]
+		#All values in p2 are valid
+		while p2Index < len(p2):
+			if p2[p2Index] not in answer: 
+				answer.append(p2[p2Index])
+			p2Index+=1
+		#print("word1 %s \t posting1:%s" %(word1, p1)) #DEBUG
+		#print("word2 %s \t posting2:%s" %(word2, p2)) #DEBUG
+	except:
+		print("%s not found in index" %(word2)) #DEBUG
+	
+
+
 
 	#if both lists were succesfully traversed and no match was made for query, return -1
 	if answer ==[]:
@@ -175,6 +186,7 @@ if __name__ == '__main__':
 	files = getAllFiles(dirName)
 	queryFile = "stemmedQuery.txt"
 	#print(files) #DEBUG
+
 	#Iterates through files and stores in Inverse Index
 	for file in files:
 		indexGenerator(dirName+file)
