6c48a34017290b61689f34f2a4180665107fb965
Sebastianxq <Sxquinones@miners.utep.edu>
2020-11-18 01:01:32 -0500

    fixed longpre issue, removed non-profs

--- InfoRetrieval/beautifulSoup/assignment2.py
+++ InfoRetrieval/beautifulSoup/assignment2.py
@@ -6,16 +6,14 @@ import time
 #get webpage and create bs object
 req = requests.get('https://www.utep.edu/cs/people/index.html')
 soup = BeautifulSoup(req.text,'lxml')
+from re import search
+
+names, titles, room, email, phone, website = [],[],[],[],[],[]
 
 #store all "div" tags in a list
 divs = soup.findAll("div", {"class":"col-md-6"})
 
-names = []
-titles = []
-room = []
-email = []
-phone = []
-website = []
+
 #iterate through tags
 for div in divs:
 
@@ -23,62 +21,66 @@ for div in divs:
   facultyTitle = div.find("span",{"class":"Title"})
   if facultyTitle is not None:
     if "Professor" in facultyTitle.text:
-      #print(facultyTitle.text)
+      print(facultyTitle.text) #DEBUG
       titles.append(facultyTitle.text)
-    else:
-      titles.append("N/A")
 
-    facultyName = div.find("h3",{"class":"name"})
-    if facultyName is not None:
-      #print(facultyName.text) #DEBUG
-      names.append(facultyName.text)
+      facultyName = div.find("h3",{"class":"name"})
+      if facultyName is not None:
+        print(facultyName.text) #DEBUG
+        names.append(facultyName.text)
 
 
-    facultyRoom = div.find("span", {"class":"address"})
-    if facultyRoom is not None:
-      #print(facultyRoom.text)
-      room.append(facultyRoom.text)
+      facultyRoom = div.find("span", {"class":"address"})
+      if facultyRoom is not None:
+        #print(facultyRoom.text)
+        room.append(facultyRoom.text)
 
-    #get professor emails
-    facultyEmail = div.find("span", {"class": "email"})
-    if facultyEmail is not None:
-      email.append(facultyEmail.text)
-      filename = facultyEmail.text[:-9] # removes "@utep.edu"
-      #print(facultyEmail.text)
-    else:
-      email.append("N/A")
+      #get professor emails
+      facultyEmail = div.find("span", {"class": "email"})
+      if facultyEmail is not None:
+        profEmail = facultyEmail.text
+        #print(profEmail)
+        email.append(profEmail)
 
-    #get professor emails
-    facultyPhone = div.find("span", {"class": "phone"})
-    if facultyPhone is not None:
-      #print(facultyPhone.text)
-      phone.append(facultyPhone.text)
-    else:
-      phone.append("N/A")
+        #due to a trailing space, longpre needs extra validation
+        if "long" in facultyEmail.text:
+          filename = "longpre"
+        else:  
+          filename = profEmail[:-9] # removes "@utep.edu"
+   
+      else:
+        email.append("N/A")
 
-    #get professors webpage (if applicable)
-    facultyWebPage = div.findAll("a")
-    facultyURL = facultyWebPage[len(facultyWebPage)-1].get("href")
+      #get professor emails
+      facultyPhone = div.find("span", {"class": "phone"})
+      if facultyPhone is not None:
+        #print(facultyPhone.text)
+        phone.append(facultyPhone.text)
+      else:
+        phone.append("N/A")
 
-    #if professor has webpage, store it in a text file
-    if len(facultyURL) > 0:
-      webPageContent = requests.get(facultyURL)
-      #print(facultyURL)
-      website.append(facultyURL)
-      content = BeautifulSoup(webPageContent.text, "lxml")
-      profFilePath = "professors/"+filename
-      file = open(profFilePath+".txt", "wt")
-      n = file.write(content.text)
-      file.close()
-    else:
-      website.append("N/A")
-    #print("=========================")
+      #get professors webpage (if applicable)
+      facultyWebPage = div.findAll("a")
+      facultyURL = facultyWebPage[len(facultyWebPage)-1].get("href")
 
+      #if professor has webpage, store it in a text file
+      if len(facultyURL) > 0:
+        webPageContent = requests.get(facultyURL)
+        #print(facultyURL)
+        website.append(facultyURL)
+        content = BeautifulSoup(webPageContent.text, "lxml")
+        profFilePath = "professors/"+filename
+        file = open(profFilePath+".txt", "wt")
+        n = file.write(content.text)
+        file.close()
+      else:
+        website.append("N/A")
+      #print("=========================")
 
 #Creates format for the dataframe and then instantiates it
 d = {'Name': names, 'Title': titles, 'Office': room, 'Email': email,'Phone': phone, 'Website': website}
 professors = pd.DataFrame(data = d)
-#print(professors)
+print(professors)
 
 professors.to_pickle("professors.pkl") #push df to pkl file
 
@@ -111,11 +113,19 @@ wordCount.sort(key = lambda x: x[0], reverse = True) #sort (rank) listings
 print("\n%d Results found in %s seconds" % (len(wordCount), time.time()-start))
 
 #Prints out ranking and attributes from the df
+#print(wordCount)
 count = 0
 for x in wordCount:
   count += 1
   fullEmail = x[1][:-4]+"@utep.edu"
-  prof = professors[professors['Email'] == fullEmail]
+  #print(fullEmail)
+
+  #Once again, the trialing space forced a workaround
+  if search("longpre",fullEmail):
+    prof = professors[professors['Email'] == "longpre@utep.edu "]
+  else:
+    prof = professors[professors['Email'] == fullEmail]
+  #print(prof)
   for index, row in prof.iterrows():
     #print(x[1]) #DEBUG
     #print(x[0])
@@ -125,20 +135,13 @@ for x in wordCount:
     print(row['Office'], row['Email'], row['Phone']+'')
     print("Website: %s\n"% (row['Website']))
 
-
-
 ##################################
 """
 TODO 
 3.)Restructure bs scrapper to be more ordered
 4.)Modularize into classes
-5.)Fix error on longpre email
-6.)Maybe look into removing those who aren't profs
-  From df
 7.)It would be cool if the final query got 
   pulled into a text file titled 
   X_Query_Results.txt
-
-
 """
 ##################################
