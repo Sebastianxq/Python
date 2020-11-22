a917cc134f94ad2dc7dd05d8dd7906a32ad7e832
Sebastianxq <Sxquinones@miners.utep.edu>
2020-11-22 17:52:37 -0500

    filtered for part b info needed

--- Networks/packetAnalysis/ccdf.py
+++ Networks/packetAnalysis/ccdf.py
@@ -2,6 +2,28 @@
 import sys, csv 
 import matplotlib.pyplot as plt
 
-plt.plot([1,2,3,4])
-plt.ylabel('some numbers')
-plt.show()
+if __name__ == '__main__':
+	packetCap = open("Netflow_dataset.csv", "r")
+	csvPacketCap = csv.reader(packetCap, delimiter=',')
+	
+	dpkts = []
+	doctets = []
+	totalTime = []
+	next(csvPacketCap) #Ignores first line of csv (col name)
+	for lines in csvPacketCap:
+		
+		dpkts.append(int(lines[4]))
+		doctets.append(int(lines[5]))
+
+		currentTime = int(lines[7])-int(lines[6])
+		#print(currentTime)
+		totalTime.append(currentTime)
+	
+	dpkts.sort(); totalTime.sort(); doctets.sort()
+
+	#print(dpkts)
+	#print(totalTime) #DEBUG
+	plt.plot(totalTime, dpkts) #format is x,y
+	#plt.ylabel('Total Packets')
+	#plt.xlabel('totalTime')
+	plt.show()
