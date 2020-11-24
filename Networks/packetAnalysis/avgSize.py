c6d809a92e7d3ffaff50be1955375dbfc1383173
Sebastianxq <Sxquinones@miners.utep.edu>
2020-11-23 19:45:58 -0500

    fixed some stuff I broke yesterday

--- Networks/packetAnalysis/avgSize.py
+++ Networks/packetAnalysis/avgSize.py
@@ -7,20 +7,19 @@ def avgSize(packetCap):
 	#NOTE: flow is the sequence of packets from src to dst
 	totalPkts,totalBytes = 0,0
 
+
 	next(packetCap) #Ignores first line of csv (col name)
 	for lines in packetCap:
-		dpkts = int(lines[4])
-		doctets = int(lines[5])
-
-		#print(dpktas)
-		#print(doctets) #DEBUG
-		totalPkts +=  int(dpkts)
+		#print(lines[5]) #DEBUG
+		
+		totalPkts +=  int(lines[4])
 		totalBytes += int(lines[5])
+
 	print("Avg bytes per packet:%d" % (totalBytes/totalPkts))
 
 if __name__ == '__main__':
 	packetCap = open("Netflow_dataset.csv", "r")
 	csvPacketCap = csv.reader(packetCap, delimiter=',')
 	
-	#part A
+
 	avgSize(csvPacketCap)
