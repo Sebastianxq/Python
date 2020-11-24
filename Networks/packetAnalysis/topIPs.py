5b6181d1733c887330dedde11ed109bcc34ad509
Sebastianxq <Sxquinones@miners.utep.edu>
2020-11-24 15:43:55 -0500

    now filtering out src IPs with no netmask

--- Networks/packetAnalysis/topIPs.py
+++ Networks/packetAnalysis/topIPs.py
@@ -5,19 +5,6 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
 	packetCap = open("Netflow_dataset.csv", "r")
 	csvPacketCap = csv.reader(packetCap, delimiter=',')
-	
-	#1.)What fraction of traffic came from 0.1% of source IP prefixes
-	#Top 1%
-	#Top 10
-	#Source Mask length=0
-	"""
-	srcaddr is 10
-	Mask length is 20
-	totalTraffic is unchanged
-
-	Once again do a dict but this time get srcaddr
-	For one instance get mask length
-	"""
 
 	
 	totalData = 0
@@ -25,15 +12,13 @@ if __name__ == '__main__':
 	next(csvPacketCap) #Ignores first line of csv (col name)
 	for lines in csvPacketCap:
 		totalData += int(lines[5])
-		#print(lines[15])
-		#print(lines[5])
+		
+		#if lines[10] in srcIPs.keys(): #ORIGINAL
 		if lines[10] in srcIPs.keys():
 			#print("dict value curr:%d" % srcDict[lines[15]])
-			#print(newValue)
 			srcIPs[lines[10]] += int(lines[5])
-			#print(srcDict)
-		else:
-			#print("adding new port")
+		elif lines[10] not in srcIPs.keys() and lines[20] == "0":
+		#else: #ORIGINAL
 			srcIPs[lines[10]] = int(lines[5])
 
 		if lines[20] in srcMask.keys():
@@ -41,31 +26,33 @@ if __name__ == '__main__':
 		else:
 			srcMask[lines[20]] = int(lines[5])
 
-	#print(totalData)
+	#print(totalData) #DEBUG
 	
 	#Sorts IPs and dumps into list
 	sortedSrc = dict(sorted(srcIPs.items(), key=lambda item: item[1]))
 	listIPs = list(sortedSrc.items())
 
 
+	#Get num of IPs in 0.1%, 1% and 10%
 	numSrcIPs = len(listIPs)
 	topPoint1 = int(numSrcIPs/1000)
-	#print(type(topPoint1)) #DEBUG
 	topOne = int(numSrcIPs/100)
 	topTen = int(numSrcIPs/10)
+	#print(type(topPoint1)) #DEBUG
+
 
 	print("total num src IPs:%d" % numSrcIPs)
 	print("0.1 contains: %d IPs" % topPoint1)
 	print("1 contains: %d IPs" % topOne)
 	print("10 contains: %d IPs\n" % topTen)
 	
+	#Creates a list containing only top X ips
 	topPoint1List = listIPs[-topPoint1:]
 	topOneList = listIPs[-topOne:]
 	topTenList = listIPs[-topTen:]
 
-	topPoint1Bytes = 0
-	topOneBytes = 0
-	topTenBytes = 0
+	topPoint1Bytes, topOneBytes,topTenBytes = 0,0,0
+
 	for ip in topPoint1List:
 		topPoint1Bytes+=ip[1]
 	print("Top 0.1 Percentage: %f" % (float(100*topPoint1Bytes/totalData)))
@@ -89,4 +76,3 @@ if __name__ == '__main__':
 			maskBytes +=maskIP[1]
 	#print(maskBytes) DEBUG
 	print("Mask Traffic Percentage: %f" % (float(100*maskBytes/totalData)))
-
